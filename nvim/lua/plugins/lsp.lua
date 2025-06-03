3ae016fff52c2b3ba4516014cc825dbb5a69cbd5
Hery Nirintsoa <hhery889@gmail.com>
2025-06-03 19:05:16 +0300

    we can add custom config for gopls for a specific projet

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -89,6 +89,22 @@ return {
 						},
 					},
 				},
+				on_new_config = function(new_config, new_root_dir)
+					if new_root_dir then
+						local gopls_config_path = new_root_dir .. "/gopls.json"
+						local file = io.open(gopls_config_path, "r")
+						if file then
+							local data = file:read("*a")
+							file:close()
+							local success, gopls_settings = pcall(vim.json.decode, data)
+							if success and gopls_settings then
+								-- Fusion des configurations
+								new_config.settings =
+									vim.tbl_deep_extend("force", new_config.settings or {}, { gopls = gopls_settings })
+							end
+						end
+					end
+				end,
 			})
 
 			local vue_language_server = mason_registry.get_package("vue-language-server"):get_install_path()
