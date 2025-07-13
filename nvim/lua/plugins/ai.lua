5b5c2a862470896324b1204c79c72d7d96af315f
Hery Nirintsoa <hhery889@gmail.com>
2025-07-13 17:51:47 +0300

    copilot

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,36 +1,33 @@
 return {
-	"github/copilot.vim",
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "main",
-		dependencies = {
-			{ "github/copilot.vim" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		build = "make tiktoken",
-		init = function()
-			vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
-		end,
-		config = function()
-			local chat = require("CopilotChat")
+	"CopilotC-Nvim/CopilotChat.nvim",
+	branch = "main",
+	dependencies = {
+		{ "github/copilot.vim" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	build = "make tiktoken",
+	init = function()
+		vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+	end,
+	config = function()
+		local chat = require("CopilotChat")
 
-			chat.setup({
-				mappings = {
-					reset = {
-						normal = "<C-x>",
-						insert = "<C-x>",
-					},
+		chat.setup({
+			mappings = {
+				reset = {
+					normal = "<C-x>",
+					insert = "<C-x>",
+				},
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>aiv", chat.toggle, { noremap = true, silent = true })
+		vim.keymap.set({ "n", "v" }, "<leader>aif", function()
+			chat.open({
+				window = {
+					layout = "float",
 				},
 			})
-
-			vim.keymap.set({ "n", "v" }, "<leader>aiv", chat.toggle, { noremap = true, silent = true })
-			vim.keymap.set({ "n", "v" }, "<leader>aif", function()
-				chat.open({
-					window = {
-						layout = "float",
-					},
-				})
-			end, { silent = true })
-		end,
-	},
+		end, { silent = true })
+	end,
 }
