67fb351b8c146347f728ed23590022f96730cc39
Hery Nirintsoa <hhery889@gmail.com>
2026-08-12 18:43:19 +0300

    vscode-js-debug remove playwright install

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"microsoft/vscode-js-debug",
-		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && rm -rf out && mv dist out && git restore package-lock.json",
+		build = "npm install --legacy-peer-deps --ignore-scripts && npx gulp vsDebugServerBundle && rm -rf out && mv dist out && git restore package-lock.json",
 	},
 	{
 		"mxsdev/nvim-dap-vscode-js",
